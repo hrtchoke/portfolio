67bf91884c74770f9a1ac2a74e1f5722a60ed1a7
hrtchoke <benxtwillxo@gmail.com>
2023-01-23 13:14:53 -0600

    added latest project

--- src/components/Posts/Projects.jsx
+++ src/components/Posts/Projects.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import twitterClone from '../../assets/img/twitterClone.png'
 import lemonade from '../../assets/img/lemonade.png'
 import hive from '../../assets/img/hive.png'
+import dropship from '../../assets/img/dropship.png'
 export default function Projects() {
     
       return (
@@ -101,12 +102,43 @@ export default function Projects() {
               </div>
             </div>
           </div>
+          <div className="card mb-3">
+            {/* row, for a row devided by 12 */}
+            <div className="row">
+              {/* col-md-4, use 4/12 width, but wrap in case ofless width than medium breakpoints */}
+              <div className="col-md-4">
+                <img src={dropship} className="card-img-top" />
+              </div>
+              {/* col-md-4, use 4/12 width, but wrap in case ofless width than medium breakpoints */}
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title"><strong>Dropshipping Store</strong></h5>
+                  <p className="card-text">
+                    <strong>Project Description:</strong> Industry-Level Dropshipping project.
+                  </p>
+                  <p className="card-text">
+                    <small className="text-muted">
+                      Test Login Account<br />
+                      Email: test@test.com<br />
+                      Pass: TEST!!
+                    </small>
+                  </p>
+                  {/* btn, button */}
+                  {/* btn-secondary, gray color button */}
+                  {/* btn-dark, dark color button */}
+                  <a href="https://dropship-frontend.hrtchoke.repl.co/" target="_blank" className="btn btn-secondary">Website</a>&nbsp;&nbsp;
+                  <a href="https://github.com/hrtchoke/dropship-frontend" target="_blank" rel="noreferrer" className="btn btn-dark">Github</a>
+                </div>
+              </div>
+            </div>
+          </div>
           <br></br>
           <br></br>
           
           
           
         </div>
+        
         
       );
     }
