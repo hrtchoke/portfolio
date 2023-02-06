142bd04b27a4ca859ecced26a83b8cb3b58e6e98
Benjamin Williams <benxtwillxo@gmail.com>
2023-02-05 20:34:39 -0600

    Delete src directory

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Router from './Router';
-import './assets/style.css';
-import style from './App.module.css';
-function App() {
-    return <Router />;
-}
-
-export default App;
